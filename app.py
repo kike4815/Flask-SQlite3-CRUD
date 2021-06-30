f370f1c75cda733ce3b94e34038580d33b484be0
kike4815 <e.pedros.sanchez@gmail.com>
2021-06-30 09:05:47 +0200

    install sqlite and create templates files

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.tasks.db'
+deb = SQLAlchemy(app)
+
+
+@app.route('/')
+def home():
+    return 'Hello word'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
